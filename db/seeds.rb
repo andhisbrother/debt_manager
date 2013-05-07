f3a1121fbc34a0c49224b4499fdeb0418e5f7d93
Tomas Stanko <tomas.stanko1000@gmail.com>
2013-05-07 20:08:58 +0200

    bar add_advanced

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Debt.create(:name => "Kreditka 1",
+			:amount => 500,
+			:repaid => 0,
+			)
